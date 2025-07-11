76024c33ebe7596689e7dddecb292603fe434e4f
xdh4uv <dhruvnainn@gmail.com>
2025-07-11 22:56:46 +0530

    kuch hua kya add

---
+++ src/main/java/com/vichitr/springboot/vchitr/VchitrApplication.java
@@ -0,0 +1,13 @@
+package com.vichitr.springboot.vchitr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VchitrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VchitrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vichitr/springboot/vchitr/VchitrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vichitr.springboot.vchitr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VchitrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
